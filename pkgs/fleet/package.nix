970cc6dfc70f92dfc66c07c0d73e189122dd6923
Adriel Velazquez <AdrielVelazquez@gmail.com>
2025-07-06 00:00:57 -0700

    Trying to reproduce the file structure

--- pkgs/fleet/package.nix
+++ pkgs/fleet/package.nix
@@ -43,14 +43,28 @@ stdenv.mkDerivation rec {
     inherit hash;
   };
 
+  # Unpack the .deb source manually since we are not using a standard source type
+  sourceRoot = ".";
+
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out
-    cp -r ./* $out
+    # Extract the debian package content into the current directory
+    dpkg-deb -x $src .
 
-    makeWrapper "$out/opt/orbit/bin/orbit/${platformDir}/stable/orbit" "$out/bin/orbit"
-    makeWrapper "$out/opt/orbit/bin/osqueryd/${platformDir}/stable/osqueryd" "$out/bin/osqueryd"
+    # Create the directory structure that orbit expects for auto-updates
+    install -d $out/opt/orbit/bin/orbit
+    install -d $out/opt/orbit/bin/osqueryd
+    install -d $out/bin
+
+    # Move the binaries from their original location in the .deb
+    # to the location expected by the application.
+    install -m 755 ./opt/orbit/bin/orbit/${platformDir}/stable/orbit $out/opt/orbit/bin/orbit/orbit
+    install -m 755 ./opt/orbit/bin/osqueryd/${platformDir}/stable/osqueryd $out/opt/orbit/bin/osqueryd/osqueryd
+
+    # Create wrappers in $out/bin pointing to the new, corrected locations
+    makeWrapper "$out/opt/orbit/bin/orbit/orbit" "$out/bin/orbit"
+    makeWrapper "$out/opt/orbit/bin/osqueryd/osqueryd" "$out/bin/osqueryd"
 
     runHook postInstall
   '';
