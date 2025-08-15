dba93ee234904ee8c89522352f785e326668ace4
Thomas Adamcik <thomas@adamcik.no>
2025-08-15 17:14:55 +0200

    Set remainder old build metadata

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,13 +4,13 @@
   src = pkgs.fetchFromGitHub {
     owner = "fleetdm";
     repo = "fleet";
-    rev = "orbit-${version}";
+    tag = "orbit-v${version}";
     sha256 = "sha256-m4cfjkwRmQM1I2ONzb6z4taOGmRVIcSLgpHaEoQIErM=";
   };
 
   vendorHash = "sha256-UOY9W2ULh2eNIfUmyU38nZCVWNTWIDTf7GBBkptrlTQ=";
-  commit = "f4389669e5181b7c92d85b4dad32fb38855fee72";
-  date = "";
+  commit = "763cb16123ec1fef7fc0926d84825742d37eb8ab";
+  date = "2025-08-11T11:03:34Z";
 
   goFlags = ["-buildvcs=false"];
   ldflags = [
@@ -18,6 +18,7 @@
     "-w"
     "-X=github.com/fleetdm/fleet/v4/orbit/pkg/build.Version=${version}"
     "-X=github.com/fleetdm/fleet/v4/orbit/pkg/build.Commit=${commit}"
+    "-X=github.com/fleetdm/fleet/v4/orbit/pkg/build.Date=${date}"
   ];
 in {
   orbit = pkgs.buildGoModule {
